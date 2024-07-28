474967e45afb9db2c2e50ad608a3d1d496775b9b
Daeyang lee <eodid007@gmail.com>
2024-07-28 16:38:26 +0900

    chore: next config images 설정 추가 (remote pattern)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/Cowokers/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
