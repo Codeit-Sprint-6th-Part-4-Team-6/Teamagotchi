372cace9d4a230a3a8f43d2c44fc54d10ec34190
Daeyang lee <eodid007@gmail.com>
2024-07-28 14:39:08 +0900

    chore: next app 재생성

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
-        port: "",
-        pathname: "/Cowokers/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
