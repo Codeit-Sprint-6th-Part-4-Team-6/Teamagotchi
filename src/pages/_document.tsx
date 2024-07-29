b0c427df19f93a84177edb5208f6bd206a8ee030
Daeyang lee <eodid007@gmail.com>
2024-07-29 15:44:48 +0900

    chore: HTML lang 변경 및 suppressHydrationWarning 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head />
-      <body>
+      <body suppressHydrationWarning>
         <Main />
         <NextScript />
       </body>
