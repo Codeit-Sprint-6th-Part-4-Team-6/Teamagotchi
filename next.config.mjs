bd50db0ea377ff44fc6bd0b2c93cfbd84c7b543d
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-07-31 15:36:32 +0900

    chore: svgr 추가 (FE-69)

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,15 @@ const nextConfig = {
       },
     ],
   },
+  
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
