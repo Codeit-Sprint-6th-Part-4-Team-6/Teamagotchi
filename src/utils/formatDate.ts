07d5c95c0a681bc5bc4ef671b67fe5f69d91dc35
임희서 <imagic@imhuiseoui-MacBookAir.local>
2024-07-31 16:51:32 +0900

    feat: added formatDate Function (FE-75)

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,15 @@
+/**
+ * 날짜 문자열을 받아 일정 형식으로 정렬합니다.
+ * @param dateString - 날짜 문자열(ex "2024-07-01T07:20:51.162Z")입니다.
+ * @param type - 리턴받을 문자열의 종류를 정합니다. default의 경우 "2024.07.01" 형식, kor의 경우 "2024년 7월 1일" 형식의 문자열을 리턴합니다. 기본값은 "default"입니다.
+ * @returns type에 따라 "2024.07.01" 형식 또는 "2024년 7월 1일" 형식의 문자열을 반환합니다.
+ */
+
+export const formatDate = (dateString: string, type: "default" | "kor" = "default"): string => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month =
+    type === "default" ? String(date.getMonth() + 1).padStart(2, "0") : String(date.getMonth() + 1);
+  const day = type === "default" ? String(date.getDate()).padStart(2, "0") : String(date.getDate());
+  return type === "default" ? `${year}.${month}.${day}` : `${year}년 ${month}월 ${day}일`;
+};
