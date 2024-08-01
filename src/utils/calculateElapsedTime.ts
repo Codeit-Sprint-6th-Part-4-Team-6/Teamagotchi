1a3edd671bf5e4973dfa6043bcaa5e93995a3234
Imgaic <162457616+Imgaic@users.noreply.github.com>
2024-08-01 13:11:19 +0900

    ✨ FEATURE: 흘러간 시간 계산, 날짜 정렬하는 유틸 함수 추가 (#11)
    * feat: added formatDate Function (FE-75)
    
    * feat: added calculateElapsedTime Function (FE-75)
    
    * feat: 윤년 고려, 유효성검사 추가, ISODateString 타입 추가 (FE-75)
    
    ---------
    
    Co-authored-by: 임희서 <imagic@imhuiseoui-MacBookAir.local>

---
+++ src/utils/calculateElapsedTime.ts
@@ -0,0 +1,37 @@
+import { ISODateString } from "@coworkers-types";
+import { formatDate } from "./formatDate";
+
+/**
+ * 전달받은 시간이 현 시점으로부터 얼마나 지났는지 계산하는 함수입니다.
+ * @param dateString - ISODateString 타입의 날짜 문자열(ex "2024-07-01T07:20:51.162Z")입니다.
+ * @returns "방금전", "1분전" ~ "2년전" 까지의 문자열을 리턴하고 3년 이상의 시간이 지났으면 "2024.07.25"와 같은 형식의 문자열을 반환합니다.
+ */
+
+export const calculateElapsedTime = (dateString: ISODateString) => {
+  const timeObject = new Date(dateString);
+  const timeInMilliseconds = timeObject.getTime();
+
+  if (Number.isNaN(timeInMilliseconds)) return "Invalid date string format";
+
+  const timeDifference = Date.now() - timeInMilliseconds;
+
+  const seconds = Math.floor(timeDifference / 1000);
+  if (seconds < 60) return "방금전";
+
+  const minutes = seconds / 60;
+  if (minutes < 60) return `${Math.floor(minutes)}분전`;
+
+  const hours = minutes / 60;
+  if (hours < 24) return `${Math.floor(hours)}시간전`;
+
+  const days = hours / 24;
+  if (days < 7) return `${Math.floor(days)}일전`;
+
+  const weeks = days / 7;
+  if (weeks < 52) return `${Math.floor(weeks)}주전`;
+
+  const years = days / 365.25; // 윤년을 고려하여 365.25로 계산
+  if (years < 3) return `${Math.floor(years)}년전`;
+
+  return formatDate(dateString);
+};
