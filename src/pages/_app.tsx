68d1aa956f15a9446821a615fdeefc1f6e8302a8
Imgaic <162457616+Imgaic@users.noreply.github.com>
2024-08-02 11:53:58 +0900

    ✨ FEATURE: Textarea 컴포넌트 추가 (#15)
    * feat: added Textarea Component (FE-68)
    
    * style: e -> event (FE-68)
    
    ---------
    
    Co-authored-by: 임희서 <imagic@imhuiseoui-MacBookAir.local>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
-import { ThemeProvider } from 'next-themes';
-import type { AppProps } from 'next/app';
-import localFont from 'next/font/local';
-
-import '@styles/globals.css';
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+import "@styles/globals.css";
 
 const pretendard = localFont({
-  src: '../../public/fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920',
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
