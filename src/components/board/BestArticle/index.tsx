627e2b270e7eb8a1f47d690486cb1b664c61ddd4
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-08-03 17:10:14 +0900

    feat: 글쓰기 버튼 추가 (FE-42)

--- src/components/board/BestArticle/index.tsx
+++ src/components/board/BestArticle/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Button from "@components/commons/Button";
 import Label from "@components/commons/Label";
 import BestArticleCard from "./BestArticlesCard";
 
@@ -18,8 +20,13 @@ interface Writer {
 
 export default function BestArticle({ Posts }: { Posts: List[] }) {
   return (
-    <div className="mx-0 my-auto flex flex-col gap-24">
-      <Label content="베스트 게시글" />
+    <div className="mx-0 my-auto flex w-full flex-col gap-12">
+      <div className="flex items-center justify-between">
+        <Label content="베스트 게시글" />
+        <Link href="/addBoard">
+          <Button size="small">글쓰기</Button>
+        </Link>
+      </div>
       <div className="flex gap-24">
         {Posts.map((item) => (
           <BestArticleCard item={item} />
