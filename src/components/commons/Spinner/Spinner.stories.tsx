3364fe68e65f2bf7ab850f685c8732cb28b7cc9e
cindyCho <hjcho2303@naver.com>
2024-08-06 16:44:09 +0900

    feat: spinner 추가 (FE-96)

---
+++ src/components/commons/Spinner/Spinner.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Spinner from "./index";
+
+const meta: Meta<typeof Spinner> = {
+  title: "Components/Spinner",
+  component: Spinner,
+  argTypes: {
+    size: {
+      control: "text",
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Size20: Story = {
+  args: {
+    size: 20,
+    color: "#10B981",
+  },
+};
+
+export const Size30: Story = {
+  args: {
+    size: 30,
+    color: "#fff",
+  },
+};
