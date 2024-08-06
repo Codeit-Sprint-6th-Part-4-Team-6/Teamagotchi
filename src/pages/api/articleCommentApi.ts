3781918f2c7ae728aee719bd33c14faa426aede4
cindyCho <hjcho2303@naver.com>
2024-08-07 07:16:54 +0900

    feat: 자유게시판 api & type 설정

---
+++ src/pages/api/articleCommentApi.ts
@@ -0,0 +1,55 @@
+import { axiosInstance } from './axios';
+import { ArticleComment, AricleCommentList, Message } from '@coworkers-types';
+
+/**
+ * 게시글의 댓글을 작성하는 API 함수입니다.
+ * @param articleId - article의 ID입니다.
+ * @param content - 전송하고자 하는 댓글 내용
+ * @returns 댓글 작성자 정보와 댓글 내용을 객체로 반환합니다. 오류 발생시 메세지 객체를 반환합니다.
+ */
+export const postArticleComment = async (articleId: number, content: string): Promise<ArticleComment | Message> => {
+  const response = await axiosInstance.post<ArticleComment | Message>(`articles/${articleId}/comments`, { content });
+  return response.data;
+};
+
+/**
+ * 게시글의 댓글 목록을 조회하는 API 함수입니다.
+ * @param articleId - article의 ID입니다.
+ * @param limit - 한 번에 조회할 댓글의 수
+ * @param cursor - 페이지네이션을 위한 커서
+ * @returns 댓글 작성자 정보와 댓글 내용이 담긴 리스트를 배열로 반환합니다. 오류 발생시 메세지 객체를 반환합니다.
+ */
+export const getArticleComments = async (
+  articleId: number,
+  limit: number,
+  cursor?: number
+): Promise<AricleCommentList | Message> => {
+  const params = new URLSearchParams({
+    limit: limit.toString(),
+    cursor: cursor ? cursor.toString() : '0',
+  });
+
+  const response = await axiosInstance.get<AricleCommentList | Message>(`articles/${articleId}/comments?${params}`);
+  return response.data;
+};
+
+/**
+ * 게시글의 댓글을 수정하는 API 함수입니다.
+ * @param commentId - comment ID입니다.
+ * @param content - 새로 수정한 댓글 내용
+ * @returns 댓글 작성자 정보와 댓글 내용을 객체로 반환합니다. 오류 발생시 메세지 객체를 반환합니다.
+ */
+export const patchArticleComment = async (commentId: number, content: string): Promise<ArticleComment | Message> => {
+  const response = await axiosInstance.patch<ArticleComment | Message>(`comments/${commentId}`, { content });
+  return response.data;
+};
+
+/**
+ * 게시글의 댓글을 삭제하는 API 함수입니다.
+ * @param commentId - 삭제하고자 하는 comment ID입니다.
+ * @returns ID를 반환합니다. 오류 발생시 메세지 객체를 반환합니다.
+ */
+export const deleteArticleComment = async (commentId: number): Promise<number | Message> => {
+  const response = await axiosInstance.patch<number | Message>(`comments/${commentId}`);
+  return response.data;
+};
