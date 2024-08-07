d80261b2327f11d0b35616755ba0911e872c314e
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-08-07 16:35:47 +0900

    feat: 소셜로그인 기능 추가 (FE-90)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,31 @@
+import { useRouter } from "next/router";
 import LoginForm from "@components/auth/LoginForm";
 import TextButton from "@components/commons/Button/TextButton";
+import SocialLoginBox from "@components/commons/SocialLoginBox";
 
+// todo: 배포하고나서 url 바꿔야됨
 export default function LoginPage() {
+  const router = useRouter();
+
+  const handleGoogleAuth = () => {
+    const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+    const redirectUri = "http://localhost:3000/oauth/google";
+    const responseType = "code";
+    const scope = process.env.NEXT_PUBLIC_GOOGLE_SCOPE;
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
+
+    router.push(url);
+  };
+
+  const handleKakaoAuth = () => {
+    const clientId = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+    const redirectUri = "http://localhost:3000/oauth/kakao";
+    const responseType = "code";
+    const url = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`;
+
+    router.push(url);
+  };
+
   return (
     <section className="mx-16 py-24 md:mx-142 md:py-100 lg:mx-auto lg:w-460">
       <h2 className="mb-24 text-center text-4xl md:mb-80">로그인</h2>
@@ -12,6 +36,11 @@ export default function LoginPage() {
           가입하기
         </TextButton>
       </div>
+      <SocialLoginBox
+        type="login"
+        onGoogleClick={handleGoogleAuth}
+        onKakaoClick={handleKakaoAuth}
+      />
     </section>
   );
 }
--- src/pages/oauth/[provider].tsx
+++ src/pages/oauth/[provider].tsx
@@ -6,7 +6,7 @@ import { socialLogin } from "../api/authApi";
 
 function OAuthRedirect() {
   const router = useRouter();
-  const { provider } = router.query;
+  const { provider, code } = router.query;
   const { setUser } = useAuthStore();
 
   useEffect(() => {
@@ -18,19 +18,20 @@ function OAuthRedirect() {
   }, [provider]);
 
   const handleRedirect = async (type: string) => {
-    const code = new URL(window.location.href).searchParams.get("code");
     if (code) {
+      // response가 실행되고 나서 리다이렉트되는 url인데 이부분을 로그인중.. 하는 페이지를 만들어서 거기로 넘겨줘야 할거같아요 아님말고 ㅎ;
+      // todo: 배포 하고나서 url 바꿔야됨
       const redirectUri = `http://localhost:3000/oauth/${type}`;
       const config = {
         redirectUri,
-        token: code,
+        token: code as string,
       };
 
       const response = await socialLogin(type.toUpperCase(), config);
       setAuth(response);
       setUser(response.user);
 
-      router.push("/login");
+      router.push("/");
     }
   };
 
