da0f8fb5a89ed062e753623cbad853923a209f89
haze <skdml20022@gmail.com>
2024-08-07 17:15:16 +0900

    fix: 소셜 로그인 기능 수정 (FE-104)

--- src/pages/oauth/[provider].tsx
+++ src/pages/oauth/[provider].tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import axios from "axios";
 import { useRouter } from "next/router";
 import { useAuthStore } from "@store/useAuthStore";
 import { setAuth } from "@utils/auth";
@@ -10,18 +11,46 @@ function OAuthRedirect() {
   const { setUser } = useAuthStore();
 
   useEffect(() => {
-    if (provider === "kakao" || provider === "google") {
-      handleRedirect(provider);
+    if (provider === "google") {
+      handleRedirectGoogle(provider);
+    } else if (provider === "kakao") {
+      handleRedirectKakao(provider);
     } else {
       router.push("/login");
     }
   }, [provider]);
 
-  const handleRedirect = async (type: string) => {
+  const handleRedirectGoogle = async (type: string) => {
     if (code) {
-      // response가 실행되고 나서 리다이렉트되는 url인데 이부분을 로그인중.. 하는 페이지를 만들어서 거기로 넘겨줘야 할거같아요 아님말고 ㅎ;
-      // todo: 배포 하고나서 url 바꿔야됨
+      // TODO: 배포 후에 url 수정 필요
       const redirectUri = `http://localhost:3000/oauth/${type}`;
+      const googleTokenResponse = await axios.post("https://oauth2.googleapis.com/token", {
+        code,
+        client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+        client_secret: process.env.NEXT_PUBLIC_GOOGLE_SECRET_KEY,
+        redirect_uri: redirectUri,
+        grant_type: "authorization_code",
+      });
+
+      const token = googleTokenResponse.data.id_token;
+
+      const response = await socialLogin(type.toUpperCase(), {
+        redirectUri,
+        token,
+      });
+
+      setAuth(response);
+      setUser(response.user);
+
+      router.push("/");
+    }
+  };
+
+  const handleRedirectKakao = async (type: string) => {
+    if (code) {
+      // TODO: 배포 후에 url 수정 필요
+      const redirectUri = `http://localhost:3000/oauth/${type}`;
+
       const config = {
         redirectUri,
         token: code as string,
