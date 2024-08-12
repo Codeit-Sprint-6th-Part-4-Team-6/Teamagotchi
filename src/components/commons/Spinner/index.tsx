bd7183c810573b03fcbeeae9eb8ca9a49354403b
cindyCho <hjcho2303@naver.com>
2024-08-12 15:33:20 +0900

    refactor: 스피너 수정 (FE-111)

--- src/components/commons/Spinner/index.tsx
+++ src/components/commons/Spinner/index.tsx
@@ -1,13 +1,18 @@
 import { ClipLoader } from "react-spinners";
 
 type Props = {
-  size: number;
-  color: string;
+  size?: number;
+  color?: "primary" | "white";
+  className?: string;
 };
-export default function Spinner({ size, color }: Props) {
+export default function Spinner({ size = 20, color = "primary", className }: Props) {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <ClipLoader size={size} color={color} speedMultiplier={0.8} />
+    <div className={`${className} flex h-full w-full items-center justify-center`}>
+      <ClipLoader
+        size={size}
+        color={color === "primary" ? "#10B981" : "#fff"}
+        speedMultiplier={0.8}
+      />
     </div>
   );
 }
