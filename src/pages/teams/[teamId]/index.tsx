4b9fd05ffa13c5e0d286039ec1b03400bdeec4ae
DAEYANG LEE <108586797+oceanlee-seoul@users.noreply.github.com>
2024-08-14 00:17:01 +0900

    ✨ FEATURE: 팀 페이지 SSR 연결 (#54)
    * feat: 팀 페이지 SSR 적용 (FE-109)
    
    * feat: react-query SSR 연결 및 notFound 처리 (FE-109)
    
    * fix: api 원복 및 쿼리 키 수정 (FE-109)

--- src/pages/teams/[teamId]/index.tsx
+++ src/pages/teams/[teamId]/index.tsx
@@ -1,8 +1,62 @@
+import {
+  DehydratedState,
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+  useQuery,
+} from "@tanstack/react-query";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
 import CircleProgressBar from "@components/TeamDetailPage/CircleProgressBar";
+import { getGroup } from "@api/groupApi";
 
-export default function TeamDetailPage() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const queryClient = new QueryClient();
+  const { teamId } = context.query;
+  const token = context.req.cookies["accessToken"];
+  // teamId가 유효한 숫자인지 확인
+  if (typeof teamId === "string") {
+    try {
+      await queryClient.fetchQuery({
+        queryKey: ["groups", teamId],
+        queryFn: () => getGroup(Number(teamId), token),
+        staleTime: Infinity,
+      });
+    } catch (error) {
+      return {
+        notFound: true,
+      };
+    }
+  }
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+export default function TeamDetailPage({ dehydratedState }: { dehydratedState: DehydratedState }) {
+  const router = useRouter();
+  const { teamId } = router.query;
+
+  const {
+    data: groupData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["groups", teamId],
+    queryFn: () => getGroup(Number(teamId)),
+    staleTime: Infinity,
+  });
+
+  if (isError) return <div>Error!</div>;
+
+  if (isLoading) return <div>Loading!</div>;
+
   return (
-    <div>
+    <HydrationBoundary state={dehydratedState}>
+      <div>태스크와 리포트, 팀원이 보이는 페이지
       <CircleProgressBar className="w-20" strokeWidth={30} progress={80} />
       <CircleProgressBar
         className="w-200"
@@ -11,6 +65,7 @@ export default function TeamDetailPage() {
         transitionDuration={1.25}
         isGradientCircle
       />
-    </div>
+      </div>
+    </HydrationBoundary>
   );
 }
