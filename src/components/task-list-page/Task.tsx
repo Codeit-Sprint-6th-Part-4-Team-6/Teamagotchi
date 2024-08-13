cfb7b241e18dcf4386416458dfa00ccffe80bba7
cindyCho <hjcho2303@naver.com>
2024-08-13 12:44:47 +0900

    feat: 할 일 완료 patch 기능 완료 (FE-54)

--- src/components/task-list-page/Task.tsx
+++ src/components/task-list-page/Task.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
-import { DateTask } from "@coworkers-types";
+import { DateTask, PatchTaskRequest } from "@coworkers-types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import classNames from "classnames";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
-import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+import EditDeletePopover from "@components/commons/Popover/EditDeletePopover";
 import {
   IconArrowReload,
   IconCalender,
   IconCheckboxActive,
   IconCheckboxDefault,
   IconComment,
-  IconKebabSmall,
   IconTime,
 } from "@utils/icon";
+import { patchTaskCompletionStatus } from "@api/taskApi";
 
 type Props = {
   task: DateTask;
@@ -26,19 +28,31 @@ const frequencyMap = {
 };
 
 export default function Task({ task }: Props) {
-  const [isChecked, setIsChecked] = useState(false);
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const { teamId, taskListsId } = router.query;
+  const [isChecked, setIsChecked] = useState(task.doneBy.user !== null);
+  const [isEditMode, setIsEditMode] = useState(false);
   const handleCheckButton = () => {
     setIsChecked((prev) => !prev);
+    taskPatchMutation.mutate({ done: !isChecked });
   };
 
   const getFrequencyText = (frequency: keyof typeof frequencyMap) =>
     frequencyMap[frequency] || frequency;
 
+  const taskPatchMutation = useMutation({
+    mutationFn: (data: PatchTaskRequest) =>
+      patchTaskCompletionStatus(teamId, taskListsId, task.id, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["taskLists"] });
+    },
+  });
+
+  const handleDelete = () => {};
+
   return (
-    <motion.div
-      whileTap={{ scale: 0.99 }}
-      className="mb-16 flex w-full cursor-pointer flex-col gap-10 rounded-8 bg-background-secondary px-14 py-12"
-    >
+    <div className="mb-16 flex w-full cursor-pointer flex-col gap-10 rounded-8 bg-background-secondary px-14 py-12">
       <div className="flex items-center justify-between gap-8">
         <div className="flex grow justify-between md:justify-start md:gap-12">
           <div className="flex items-center gap-8">
@@ -54,7 +68,11 @@ export default function Task({ task }: Props) {
             <span className="text-12 text-text-default">{task.commentCount}</span>
           </span>
         </div>
-        <IconKebabSmall />
+        <EditDeletePopover
+          icon="kebabSmall"
+          handleModify={() => setIsEditMode(true)}
+          handleDelete={handleDelete}
+        />
       </div>
 
       <div className="flex items-center gap-10">
@@ -79,6 +97,6 @@ export default function Task({ task }: Props) {
           </span>
         </span>
       </div>
-    </motion.div>
+    </div>
   );
 }
