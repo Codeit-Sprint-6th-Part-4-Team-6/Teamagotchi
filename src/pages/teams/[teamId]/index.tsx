2c6159e4a143a6033f877345d64922d951e48f40
DAEYANG LEE <108586797+oceanlee-seoul@users.noreply.github.com>
2024-08-14 00:13:51 +0900

    ✨ FEATURE: CircleProgressBar (#53)
    * feat: circleProgressBar 추가 및 테스트 코드 추가 (FE-110)
    
    * fix: 필요 없는 props 제거 (FE-110)
    
    * fix: 프롭스 추가 제거 (FE-110)

--- src/pages/teams/[teamId]/index.tsx
+++ src/pages/teams/[teamId]/index.tsx
@@ -1,3 +1,16 @@
+import CircleProgressBar from "@components/TeamDetailPage/CircleProgressBar";
+
 export default function TeamDetailPage() {
-  return <div>태스크와 리포트, 팀원이 보이는 페이지</div>;
+  return (
+    <div>
+      <CircleProgressBar className="w-20" strokeWidth={30} progress={80} />
+      <CircleProgressBar
+        className="w-200"
+        strokeWidth={35}
+        progress={30}
+        transitionDuration={1.25}
+        isGradientCircle
+      />
+    </div>
+  );
 }
