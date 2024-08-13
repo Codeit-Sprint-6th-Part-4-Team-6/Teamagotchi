d36b4a4b1abad4af4f33dd6692199c517f053914
Daeyang lee <eodid007@gmail.com>
2024-08-13 16:40:16 +0900

    fix: 필요 없는 props 제거 (FE-110)

--- src/components/TeamDetailPage/CircleProgressBar.tsx
+++ src/components/TeamDetailPage/CircleProgressBar.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // Props 타입 정의
 interface ProgressProps {
   progress?: number; // 진행률 (0 ~ 100)
   strokeWidth?: number; // 원형 경로의 두께
-  ballStrokeWidth?: number; // 끝에 위치하는 공의 두께
-  reduction?: number; // 원형의 크기를 줄이는 비율
   transitionDuration?: number; // 애니메이션 지속 시간 (초)
   transitionTimingFunction?: string; // 애니메이션 타이밍 함수
   background?: string; // 배경 원형 색상
@@ -17,14 +15,24 @@ interface ProgressProps {
 function CircleProgressBar({
   progress = 0, // 기본 진행률은 0%
   strokeWidth = 4, // 기본 원형 경로의 두께는 4px
-  ballStrokeWidth = 16, // 기본 공의 두께는 16px
-  reduction = 0.25, // 기본 원형 크기 비율은 0.25
   transitionDuration = 0.5, // 기본 애니메이션 지속 시간은 0.5초
   transitionTimingFunction = "ease", // 기본 애니메이션 타이밍 함수는 "ease"
   background = "#dde2e9", // 기본 배경 원형 색상은 회색
   className, // 사용자 정의 CSS 클래스 적용
   isGradient = false, // 기본적으로 그라데이션은 사용하지 않음
 }: ProgressProps) {
+  const [displayProgress, setDisplayProgress] = useState(0);
+
+  // 애니메이션을 위한 useEffect
+  useEffect(() => {
+    // 500ms 후에 진행률 설정
+    const timeout = setTimeout(() => {
+      setDisplayProgress(progress);
+    }, 500); // 500ms 후에 진행률 업데이트
+
+    return () => clearTimeout(timeout); // 클린업
+  }, [progress]);
+
   // isGradient가 true일 경우, 기본 그라데이션 색상 배열 설정
   const appliedGradient = isGradient
     ? [
@@ -36,13 +44,14 @@ function CircleProgressBar({
         { stop: 1, color: "#10B982" }, // 기본 색상 사용
       ];
 
-  const roundedProgress = Math.round(progress * 100) / 100; // 진행률 소수점 두 자리로 반올림
+  const reduction = 2;
+  const roundedProgress = Math.round(displayProgress * 100) / 100; // 진행률 소수점 두 자리로 반올림
   const width = 200; // SVG의 너비
   const center = width / 2; // SVG의 중심 좌표
   const height = 200 || center + center * Math.cos(reduction * Math.PI); // SVG의 높이 계산
   const [unique] = useState(() => Math.random().toString()); // 유니크한 ID 생성
   const rotate = 90 + 180 * reduction; // 원형의 회전 각도 계산
-  const r = center - strokeWidth / 2 - ballStrokeWidth / 2; // 원형의 반지름 계산
+  const r = center - strokeWidth / 2 - 16 / 2; // 원형의 반지름 계산
   const circumference = Math.PI * r * 2; // 원형의 둘레 계산
   const offset = (circumference * (100 - roundedProgress * (1 - reduction))) / 100; // 진행률에 따른 원형의 오프셋 계산
 
@@ -90,6 +99,5 @@ function CircleProgressBar({
     </div>
   );
 }
-
 // React.memo를 사용하여 불필요한 재렌더링을 방지
 export default React.memo(CircleProgressBar);
