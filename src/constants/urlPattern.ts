51de9e361af6f496668f7a619789c4483c335a3a
Imgaic <162457616+Imgaic@users.noreply.github.com>
2024-08-14 14:27:45 +0900

    ✨ FEATURE: JoinTeamPage (#55)
    * feat: groupPreview 기능, 초대 링크 생성 기능 추가 (FE-80)
    
    * feat: 팀 참여하기 기능 완성 (FE-80)
    
    * style: 에러메시지 한글화 (FE-80)
    
    ---------
    
    Co-authored-by: 임희서 <imagic@imhuiseoui-MacBookAir.local>

--- src/constants/urlPattern.ts
+++ src/constants/urlPattern.ts
@@ -1,2 +1,2 @@
 export const urlPattern =
-  /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
+  /(\b(https?|http|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/i;
