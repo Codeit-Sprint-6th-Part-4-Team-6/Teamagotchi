76d12e75eb9056cb3575e3a4f0134e2f4b4207c8
cindyCho <hjcho2303@naver.com>
2024-08-14 15:34:36 +0900

    remove: 컴포넌트 분리로 인한 코드 지우기 (FE-54)

--- src/components/task-list-page/DateWithCalendar.tsx
+++ src/components/task-list-page/DateWithCalendar.tsx
@@ -1,14 +1,10 @@
-import { ChangeEvent, useState } from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addDays, format } from "date-fns";
 import { ko } from "date-fns/locale";
-import { useRouter } from "next/router";
 import TextButton from "@components/commons/Button/TextButton";
 import Popover from "@components/commons/Popover";
-import OneInputModal from "@components/commons/modal/OneInputModal";
+import AddTaskListModal from "@components/commons/modal/AddTaskListModal";
 import { useModal } from "@hooks/useModal";
 import { IconArrowLeftBg, IconArrowRightBg, IconCalenderBg } from "@utils/icon";
-import { postTaskList } from "@api/taskListApi";
 import Calendar from "./Calendar";
 
 type Props = {
@@ -55,43 +51,5 @@ export default function DateWithCalendar({ date, onDateChange }: Props) {
         새로운 목록 추가하기
       </TextButton>
     </div>
-  );
-}
-
-function AddTaskListModal({ onClose }: { onClose?: () => void }) {
-  const { closeModal } = useModal();
-
-  const queryClient = useQueryClient();
-  const router = useRouter();
-  const { teamId } = router.query;
-  const [taskName, setTaskName] = useState("");
-  const handleTaskName = (event: ChangeEvent<HTMLInputElement>) => {
-    setTaskName(event.target.value);
-  };
-
-  const handleConfirm = () => {
-    postTaskListMutation.mutate();
-  };
-
-  const postTaskListMutation = useMutation({
-    mutationFn: () => postTaskList(teamId, taskName),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["group"] });
-      closeModal();
-    },
-  });
-
-  return (
-    <OneInputModal
-      title="새로운 목록 추가"
-      content="할 일에 대한 목록을 추가하고<br> 목록별 할 일을 만들 수 있습니다."
-      placeholder="목록 이름을 입력해주세요."
-      buttonText="만들기"
-      onConfirm={handleConfirm}
-      onClose={onClose}
-      value={taskName}
-      onChange={handleTaskName}
-      isPending={postTaskListMutation.isPending}
-    />
   );
 }
