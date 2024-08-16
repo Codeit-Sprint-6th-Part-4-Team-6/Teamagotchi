3791be329b098c01a56342e1455515cf1f793c78
Daeyang lee <eodid007@gmail.com>
2024-08-16 13:27:58 +0900

    feat: 할 일이 없을 때 조건부 렌더링 (FE-61)

--- src/components/TeamDetailPage/TaskListSection.tsx
+++ src/components/TeamDetailPage/TaskListSection.tsx
@@ -17,12 +17,16 @@ export default function TaskListSection({
           <Label content="할 일 목록" />
           <span className="text-lg font-normal text-text-default">{`(${taskLists.length}개)`}</span>
         </div>
-        {role === "ADMIN" ? <TextButton icon="plus">새로운 목록 추가하기</TextButton> : null}
+        {role === "ADMIN" && <TextButton icon="plus">새로운 목록 추가하기</TextButton>}
       </div>
       <div className="mb-20 flex flex-col gap-16">
-        {taskLists.map((taskList, index) => (
-          <TaskListItem key={taskList.id} task={taskList} index={index} role={role} />
-        ))}
+        {taskLists.length > 0 ? (
+          taskLists.map((taskList, index) => (
+            <TaskListItem key={taskList.id} task={taskList} index={index} role={role} />
+          ))
+        ) : (
+          <p className="my-30 text-center text-md text-text-default">아직 할 일 목록이 없습니다</p>
+        )}
       </div>
     </section>
   );
