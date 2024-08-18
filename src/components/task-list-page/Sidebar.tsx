0b7e16cabc17e8dbc5a10e687d39e48ef4e785ef
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-08-18 23:10:55 +0900

    style: 댓글달기 디자인 구현 (FE-55)

--- src/components/task-list-page/Sidebar.tsx
+++ src/components/task-list-page/Sidebar.tsx
@@ -1,13 +1,22 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { TaskCommentList, TaskDetails } from "@coworkers-types";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import classNames from "classnames";
 import { format } from "date-fns";
 import Button from "@components/commons/Button";
 import Comment from "@components/commons/Comment";
+import Input from "@components/commons/Input";
 import NameTag from "@components/commons/NameTag";
 import EditDeletePopover from "@components/commons/Popover/EditDeletePopover";
-import { IconCalender, IconClose, IconRepeat, IconTime } from "@utils/icon";
+import Textarea from "@components/commons/TextArea";
+import {
+  IconCalender,
+  IconClose,
+  IconEnterActive,
+  IconEnterDefault,
+  IconRepeat,
+  IconTime,
+} from "@utils/icon";
 import { getTaskDetails } from "@api/taskApi";
 import { getTaskComments } from "@api/taskCommentApi";
 
@@ -23,6 +32,7 @@ export default function Sidebar({
   onClose: Dispatch<SetStateAction<boolean>>;
 }) {
   // const side = useRef();
+  const [textareaComment, setTextareaComment] = useState<string>("");
 
   const { data: TaskDetailData } = useQuery<TaskDetails>({
     queryKey: ["taskListDetail", taskId],
@@ -73,12 +83,18 @@ export default function Sidebar({
     }
   }
 
+  const handleChangeTextarea = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextareaComment(event.target.value);
+  };
+
   const formatClass = classNames("text-text-default text-12");
 
   const lineClass = classNames("h-10 border border-l border-solid border-background-tertiary");
 
+  const postIconClass = classNames("absolute right-0 top-13 cursor-pointer");
+
   return (
-    <div className="h-full w-full bg-background-secondary md:fixed md:right-0 md:top-61 md:w-435 lg:w-779">
+    <div className="z-[999] h-full w-full bg-background-secondary md:fixed md:right-0 md:top-61 md:w-435 lg:w-779">
       <div className="flex h-full flex-col gap-16 p-24">
         <IconClose className="cursor-pointer" onClick={onClose} />
         <div className="flex justify-between">
@@ -104,6 +120,20 @@ export default function Sidebar({
           <p className={formatClass}>{frequency}</p>
         </div>
         <div className="h-200 overflow-y-scroll">{TaskDetailData?.description}</div>
+        <div className="relative">
+          <Textarea
+            type="innerButton"
+            placeholder="댓글을 달아주세요."
+            height={50}
+            value={textareaComment}
+            onChange={handleChangeTextarea}
+          />
+          {textareaComment.length > 0 ? (
+            <IconEnterActive className={postIconClass} />
+          ) : (
+            <IconEnterDefault className={postIconClass} />
+          )}
+        </div>
         {TaskCommentData?.map((comment) => <Comment type="task" comment={comment} />)}
         <Button
           buttonType="floating"
