64bdfcd0eb4f613564b6d7da20c99ae5c56f0b53
Daeyang lee <eodid007@gmail.com>
2024-08-20 17:22:44 +0900

    feat: sidebar overlay 추가 및 바깥 쪽 스크롤 중지 (FE-129)

--- src/components/Header/Sidebar.tsx
+++ src/components/Header/Sidebar.tsx
@@ -1,7 +1,9 @@
+import { useEffect } from "react";
 import { Membership } from "@coworkers-types";
 import classNames from "classnames";
 import Image from "next/image";
 import Link from "next/link";
+import useMediaQuery from "@hooks/useMediaQuery";
 
 export function Sidebar({
   sidebarOpen,
@@ -12,34 +14,64 @@ export function Sidebar({
   setSidebarOpen: (open: boolean) => void;
   groups: Membership[] | undefined;
 }) {
+  const { isDesktop, isTablet } = useMediaQuery();
+
+  useEffect(() => {
+    // 화면 크기가 Desktop 또는 Tablet으로 변경되면 사이드바를 닫음
+    if ((isDesktop || isTablet) && sidebarOpen) {
+      setSidebarOpen(false);
+    }
+  }, [isDesktop, isTablet, sidebarOpen, setSidebarOpen]);
+
+  useEffect(() => {
+    // sidebar가 열렸을 때 바깥쪽 scroll 중지
+    if (sidebarOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [sidebarOpen]);
+
   return (
-    <div
-      className={classNames(
-        "absolute left-0 top-0 z-50 h-[100vh] w-204 bg-background-tertiary p-16 transition-transform duration-300 md:hidden",
-        {
-          "translate-x-0 transform": sidebarOpen,
-          "-translate-x-full transform": !sidebarOpen,
-        }
+    <>
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 z-40 bg-[#00000050] md:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
       )}
-    >
-      <div className="mb-24 flex w-full justify-end">
-        <button onClick={() => setSidebarOpen(false)}>
-          <Image src="/icons/icon_close.svg" alt="사이드바 닫기 버튼" width={24} height={24} />
-        </button>
-      </div>
-      <div className="flex flex-col gap-24">
-        <Link href="/board" className="text-md">
-          자유게시판
-        </Link>
-        <Link href="/teams" className="text-md">
-          팀 리스트
-        </Link>
-        {groups?.map((group) => (
-          <Link className="text-md" href={`/teams/${group.groupId}`} key={group.groupId}>
-            {group.group.name}
+
+      <div
+        className={classNames(
+          "fixed left-0 top-0 z-50 h-[100vh] w-204 bg-background-tertiary p-16 transition-transform duration-300 md:hidden",
+          {
+            "translate-x-0 transform": sidebarOpen,
+            "-translate-x-full transform": !sidebarOpen,
+          }
+        )}
+      >
+        <div className="mb-24 flex w-full justify-end">
+          <button onClick={() => setSidebarOpen(false)}>
+            <Image src="/icons/icon_close.svg" alt="사이드바 닫기 버튼" width={24} height={24} />
+          </button>
+        </div>
+        <div className="flex flex-col gap-24">
+          <Link href="/board" className="text-md">
+            자유게시판
           </Link>
-        ))}
+          <Link href="/teams" className="text-md">
+            팀 리스트
+          </Link>
+          {groups?.map((group) => (
+            <Link className="text-md" href={`/teams/${group.groupId}`} key={group.groupId}>
+              {group.group.name}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
