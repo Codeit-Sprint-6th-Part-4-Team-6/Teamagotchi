c82589b414c304ad260864850ca123b637abf6ad
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-08-21 19:37:55 +0900

    style: 모달 디자인 수정 및 애니메이션 추가 (FE-134)

--- src/components/commons/modal/ModalWrapper.tsx
+++ src/components/commons/modal/ModalWrapper.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import classNames from "classnames";
 import useMediaQuery from "@hooks/useMediaQuery";
 import { useModal } from "@hooks/useModal";
 
@@ -22,12 +23,16 @@ export default function ModalWrapper() {
     onClose: closeModal,
   };
 
+  const modalClass = classNames(
+    "modal-container-base transition-transform",
+    isMobile
+      ? `modal-container-mobile duration-500 ${isVisible ? `-translate-y-0` : `translate-y-300`}`
+      : `modal-container duration-500 ease-in-out ${isVisible ? `scale-1` : `scale-0`}`
+  );
+
   return (
     <div className="modal-overlay" onClick={closeModal}>
-      <div
-        className={`modal-container transition-transform duration-500 ease-in-out ${isVisible ? `scale-1` : `scale-0`}`}
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className={modalClass} onClick={(e) => e.stopPropagation()}>
         <ModalComponent {...enhancedModalProps} />
       </div>
     </div>
