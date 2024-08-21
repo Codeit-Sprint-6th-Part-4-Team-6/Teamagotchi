b34c7946b935b16b7284faeeec2822ee90d31cd7
Daeyang lee <eodid007@gmail.com>
2024-08-21 22:14:09 +0900

    feat: sidebar 디자인 변경 (FE-133)

--- src/components/Header/Sidebar.tsx
+++ src/components/Header/Sidebar.tsx
@@ -46,7 +46,7 @@ export function Sidebar({
 
       <div
         className={classNames(
-          "fixed left-0 top-0 z-50 h-[100vh] w-204 bg-background-tertiary p-16 transition-transform duration-300 md:hidden",
+          "fixed left-0 top-0 z-50 h-[100vh] w-204 bg-background-secondary p-16 transition-transform duration-300 md:hidden",
           {
             "translate-x-0 transform": sidebarOpen,
             "-translate-x-full transform": !sidebarOpen,
@@ -58,18 +58,32 @@ export function Sidebar({
             <Image src="/icons/icon_close.svg" alt="사이드바 닫기 버튼" width={24} height={24} />
           </button>
         </div>
-        <div className="flex flex-col gap-24">
-          <Link href="/board" className="text-md">
+        <div className="flex h-[90%] flex-col gap-5">
+          <hr className="h-1 border-0 bg-background-tertiary" />
+          <Link
+            href="/board"
+            className="rounded-8 p-16 text-md transition-all hover:bg-background-tertiary"
+          >
             자유게시판
           </Link>
-          <Link href="/teams" className="text-md">
+          <Link
+            href="/teams"
+            className="rounded-8 p-16 text-md transition-all hover:bg-background-tertiary"
+          >
             팀 리스트
           </Link>
-          {groups?.map((group) => (
-            <Link className="text-md" href={`/teams/${group.groupId}`} key={group.groupId}>
-              {group.group.name}
-            </Link>
-          ))}
+          <hr className="h-1 border-0 bg-background-tertiary" />
+          <div className="flex flex-col overflow-y-scroll">
+            {groups?.map((group) => (
+              <Link
+                className="rounded-8 p-16 text-md transition-all hover:bg-background-tertiary"
+                href={`/teams/${group.groupId}`}
+                key={group.groupId}
+              >
+                {group.group.name}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </>
