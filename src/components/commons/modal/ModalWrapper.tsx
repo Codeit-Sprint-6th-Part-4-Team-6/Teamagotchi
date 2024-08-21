b787ee7ee7a11890c01644faed858b6d88d9a693
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-08-21 19:24:09 +0900

    style: 모달 열때 애니메이션 추가 (FE-134)

--- src/components/commons/modal/ModalWrapper.tsx
+++ src/components/commons/modal/ModalWrapper.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useMediaQuery from "@hooks/useMediaQuery";
 import { useModal } from "@hooks/useModal";
 
 export default function ModalWrapper() {
   const { modalType, ModalComponent, modalProps, closeModal } = useModal();
+  const { isMobile } = useMediaQuery();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (modalType && ModalComponent) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }, [modalType, ModalComponent]);
 
   if (!modalType || !ModalComponent) return null;
 
@@ -13,7 +24,10 @@ export default function ModalWrapper() {
 
   return (
     <div className="modal-overlay" onClick={closeModal}>
-      <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+      <div
+        className={`modal-container transition-transform duration-500 ease-in-out ${isVisible ? `scale-1` : `scale-0`}`}
+        onClick={(e) => e.stopPropagation()}
+      >
         <ModalComponent {...enhancedModalProps} />
       </div>
     </div>
