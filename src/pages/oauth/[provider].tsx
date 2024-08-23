ba56a6d7f85ec098ea866afe021a86159cf66724
임희서 <imagic@imhuiseoui-MacBookAir.local>
2024-08-23 21:35:25 +0900

    fix: added slash (FE-143)

--- src/pages/oauth/[provider].tsx
+++ src/pages/oauth/[provider].tsx
@@ -22,7 +22,7 @@ function OAuthRedirect() {
 
   const handleRedirectGoogle = async (type: string) => {
     if (code) {
-      const redirectUri = `${process.env.NEXT_PUBLIC_SITE_URL}oauth/${type}`;
+      const redirectUri = `${process.env.NEXT_PUBLIC_SITE_URL}/oauth/${type}`;
       const googleTokenResponse = await axios.post("https://oauth2.googleapis.com/token", {
         code,
         client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
@@ -44,7 +44,7 @@ function OAuthRedirect() {
 
   const handleRedirectKakao = async (type: string) => {
     if (code) {
-      const redirectUri = `${process.env.NEXT_PUBLIC_SITE_URL}oauth/${type}`;
+      const redirectUri = `${process.env.NEXT_PUBLIC_SITE_URL}/oauth/${type}`;
 
       const config = {
         redirectUri,
