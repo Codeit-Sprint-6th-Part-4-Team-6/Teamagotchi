0abc619ce9d7f3e84605146029bb55146c0db720
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-08-23 21:40:55 +0900

    style: 아티클카드 디자인 수정 (FE-142)

--- src/components/board/ArticleSection/ArticleCard.tsx
+++ src/components/board/ArticleSection/ArticleCard.tsx
@@ -1,13 +1,13 @@
 import { Article } from "@coworkers-types";
-import { format } from "date-fns";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import NameTag from "@components/commons/NameTag";
+import { calculateElapsedTime } from "@utils/calculateElapsedTime";
 import { IconComment, IconHeart } from "@utils/icon";
 
 export default function ArticleCard({ Post }: { Post: Article }) {
-  const { id, title, image, createdAt, writer, likeCount } = Post;
+  const { id, title, image, createdAt, writer, likeCount, commentCount } = Post;
   return (
     <Link href={`/board/${id}`}>
       <motion.div
@@ -15,27 +15,29 @@ export default function ArticleCard({ Post }: { Post: Article }) {
         transition={{ duration: 0.2 }}
         className="mb-24 flex h-176 w-full flex-col justify-between rounded-8 bg-background-secondary p-24"
       >
-        <div className="flex justify-between">
+        <div className="relative flex justify-between">
           <span className="truncate text-18 font-[500] text-text-primary">{title}</span>
           {image && <Image width={72} height={72} alt="image" src={image} />}
         </div>
         <div className="flex justify-between">
           <div className="flex items-center justify-center gap-12">
             <NameTag type="default-12" image={writer.image} name={writer.nickname} />
-            <div className="border-l border-solid border-background-tertiary pl-12">
+          </div>
+          <div>
+            <div className="flex justify-end">
               <span className="text-14 font-[500] text-text-disabled">
-                {format(createdAt, "yyyy.MM.dd")}
+                {calculateElapsedTime(createdAt)}
               </span>
             </div>
-          </div>
-          <div className="flex gap-12">
-            <div className="flex items-center justify-center gap-5">
-              <IconComment />
-              <span className="text-14 font-[400] text-[#94A3B8]">{likeCount}</span>
-            </div>
-            <div className="flex items-center justify-center gap-5">
-              <IconHeart />
-              <span className="text-14 font-[400] text-[#94A3B8]">{likeCount}</span>
+            <div className="flex gap-12">
+              <div className="flex items-center justify-center gap-5">
+                <IconComment />
+                <span className="text-14 font-[400] text-[#94A3B8]">{commentCount}</span>
+              </div>
+              <div className="flex items-center justify-center gap-5">
+                <IconHeart />
+                <span className="text-14 font-[400] text-[#94A3B8]">{likeCount}</span>
+              </div>
             </div>
           </div>
         </div>
