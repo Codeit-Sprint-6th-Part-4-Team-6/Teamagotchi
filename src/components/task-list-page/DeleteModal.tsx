7f9ce4349945ec81856b49f55edafa15c566b2d3
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-08-25 21:20:23 +0900

    ♻️ REFACTOR: mobile refactor (#87)
    * style: 마진 추가 (FE-142)
    
    * style: 모바일 버튼 위치 조정 (FE-142)
    
    * style: 사이드바 스타일 변경 (FE-142)
    
    * refactor: setUser 함수 추가 (FE-142)
    
    * style: 버튼 모바일 반응형 추가 (FE-142)
    
    * style: 사이드바 디스크립션 최소높이 (FE-142)
    
    * style: 모달 모바일 디자인 수정 (FE-142)
    
    * style: 아티클카드 디자인 수정 (FE-142)
    
    * refactor: 페이지네이션 수정 (FE-142)
    
    * style: 상세보기 페이지 디자인 수정 (FE-142)
    
    * feat: 할일 수정, 삭제 기능 추가 (FE-142)
    
    * refactor: 안쓰는 코드 삭제 (FE-142)
    
    * refactor: router 유효성 검사 추가 (FE-142)
    
    * refactor: router 유효성 검사 로직 수정 (FE-142)
    
    * refactor: 코드 순서 수정 (FE-142)
    
    * refactor: 페이지네이션 훅 삭제 및 코드통합 (FE-142)
    
    * feat: orderBy 유효성 검사 추가 (FE-142)

--- src/components/task-list-page/DeleteModal.tsx
+++ src/components/task-list-page/DeleteModal.tsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 import Spinner from "@components/commons/Spinner";
 import WarnModal from "@components/commons/modal/WarnModal";
 import { useToast } from "@hooks/useToast";
@@ -7,18 +8,23 @@ import { deleteTask } from "@api/taskApi";
 export default function DeleteModal({
   onClose,
   taskId,
+  type = "tasklist",
 }: {
   onClose?: () => void;
   taskId?: string;
+  type?: "tasklist" | "sidebar";
 }) {
   const queryClient = useQueryClient();
   const { toast } = useToast();
+  const router = useRouter();
 
   const taskDeleteMutation = useMutation({
     mutationFn: () => deleteTask(taskId),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["taskList"] });
+      queryClient.invalidateQueries({ queryKey: ["taskListDetail", Number(taskId)] });
       onClose?.();
+      if (type === "sidebar") router.reload();
     },
     onError: () => {
       toast("danger", "할 일 삭제에 실패했습니다. 다시 시도해 주세요.");
