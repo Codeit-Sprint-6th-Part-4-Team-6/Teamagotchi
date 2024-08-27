f041ad3fe813087f05b3178d2e1802e95c419641
haze <skdml20022@gmail.com>
2024-08-27 20:59:31 +0900

    feat: route 로딩 처리 추가 (FE-154)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
+import { useEffect, useState } from "react";
 import { HydrationBoundary, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "@components/Header";
+import Loading from "@components/commons/LottieAnimation/Loading";
 import Toast from "@components/commons/Toast";
 import ModalWrapper from "@components/commons/modal/ModalWrapper";
 import "@styles/globals.css";
@@ -17,6 +20,24 @@ const pretendard = localFont({
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => setLoading(true);
+    const handleRouteChangeComplete = () => setLoading(false);
+
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    router.events.on("routeChangeError", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      router.events.off("routeChangeError", handleRouteChangeComplete);
+    };
+  }, [router]);
+
   return (
     <>
       <Head>
@@ -26,6 +47,11 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <QueryClientProvider client={queryClient}>
         <main className={pretendard.className}>
+          {loading && (
+            <div className="fixed z-50 h-full w-screen bg-[#111]">
+              <Loading />
+            </div>
+          )}
           <HydrationBoundary state={pageProps.dehydratedState}>
             <Header />
             <ModalWrapper />
