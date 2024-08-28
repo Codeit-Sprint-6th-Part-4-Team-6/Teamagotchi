8d221299597b4876ec5145d3601ba45ccdce7c6d
Hyunji Cho <123517278+cindycho0423@users.noreply.github.com>
2024-08-28 14:25:07 +0900

    ♻️ REFACTOR : error-boundary 적용 (#97)
    * style: pl 추가 (FE-151)
    
    * style: css 수정 (FE-151)
    
    * refactor: 팀페이지 에러바운더리 적용 (FE-151)
    
    * remove: 필요하지 않은 파일 삭제 (FE-151)

--- src/components/teams/TeamList.tsx
+++ src/components/teams/TeamList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Membership } from "@coworkers-types";
 import { useQuery } from "@tanstack/react-query";
+import ErrorBoundary from "@components/commons/ErrorBoundary";
 import LottieAnimation from "@components/commons/LottieAnimation";
 import Spinner from "@components/commons/Spinner";
 import { getUserMemberships } from "@api/userApi";
@@ -27,25 +28,27 @@ export default function TeamList() {
 
   return (
     <>
-      {teamList && teamList.length !== 0 ? (
-        <div className="md:min-h-200">
-          <ul className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
-            {teamList.map((data: Membership) => (
-              <TeamItem key={data.groupId} data={data} />
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div className="mt-100">
-          <LottieAnimation type="success" size={100} />
-          <p className="mt-32 text-center text-14 text-text-default md:mt-48">
-            아직 소속된 팀이 없습니다.
-          </p>
-          <p className="text-center text-14 text-text-default">
-            팀을 생성하거나 팀에 참여해보세요.
-          </p>
-        </div>
-      )}
+      <ErrorBoundary queryKey={{ queryKey: ["groups"] }}>
+        {teamList && teamList.length !== 0 ? (
+          <div className="md:min-h-200">
+            <ul className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+              {teamList.map((data: Membership) => (
+                <TeamItem key={data.groupId} data={data} />
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div className="mt-100">
+            <LottieAnimation type="success" size={100} />
+            <p className="mt-32 text-center text-14 text-text-default md:mt-48">
+              아직 소속된 팀이 없습니다.
+            </p>
+            <p className="text-center text-14 text-text-default">
+              팀을 생성하거나 팀에 참여해보세요.
+            </p>
+          </div>
+        )}
+      </ErrorBoundary>
     </>
   );
 }
