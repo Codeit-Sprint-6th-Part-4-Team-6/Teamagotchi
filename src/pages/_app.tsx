5f857681800e9fd55b61b478eca58e793e663f7d
cindyCho <hjcho2303@naver.com>
2024-09-02 14:10:10 +0900

    fix: og image 수정 (FE-161)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -50,7 +50,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>티마고치</title>
         <meta name="description" content="팀원들과 함께 팀을 키우고 관리하는 재미를 느껴보세요!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:image" content="@images/metadata.png" />
       </Head>
       <QueryClientProvider client={queryClient}>
         <main className={pretendard.className}>
